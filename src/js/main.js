e501e39b8537287829476485f1d34188e26459c0
obsujdaem <obsujdaemdota2@gmail.com>
2019-07-07 04:24:53 +0300

    hero end

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,15 @@
 'use strict';
 
 ;(function() {
-    
+    $('.js-hero-slider').slick({
+        autoplay: true,
+        arrows: false,
+        dots: true,
+        fade: true,
+        speed: 3500,
+        autoplaySpeed: 4500,
+        customPaging : function(slider, i) {
+            return '<span class="slider-dot"></span>';
+        },
+    });
 })();
