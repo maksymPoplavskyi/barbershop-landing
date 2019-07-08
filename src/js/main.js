d17c47b5a805b3b2d2e4439333f6cfa224dd4962
obsujdaem <obsujdaemdota2@gmail.com>
2019-07-09 02:38:48 +0300

    end reviews

--- src/js/main.js
+++ src/js/main.js
@@ -12,16 +12,27 @@
             return '<span class="slider-dot"></span>';
         },
     });
-})();
 
-var myPlayer;
-jQuery(function () {
+    var myPlayer;
+
+$(function() {
 
     var options = {
         mobileFallbackImage: "http://www.hdwallpapers.in/walls/pink_cosmos_flowers-wide.jpg",
         playOnlyIfVisible  : false
     };
 
-myPlayer = jQuery(".player").YTPlayer(options);
+myPlayer = $(".player").YTPlayer(options);
 
 });
+
+    $('.people-item__btn').on('click', fToggle);
+
+function fToggle() {
+    $('.people-item__commentary')
+        .not($(this).next() )
+        .slideUp(300);
+    $(this).next().slideToggle(300);
+}
+
+})();
